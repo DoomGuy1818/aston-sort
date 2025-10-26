baaeeae22b77764ac37c0cc612b504d61c7e3107
Zhonya <lxcksmitaqukillacurry@gmail.com>
2025-10-26 21:09:06 +0300

    Добавил класс студента из другой ветки

--- src/main/java/search/student/Student.java
+++ src/main/java/search/student/Student.java
@@ -1,4 +1,76 @@
 package search.student;
 
-public class Student {
+import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Потока независимый class Student
+ */
+public class Student implements Comparable<Student> {
+    // Общий счётчик студентов
+    private static AtomicInteger nextId = new AtomicInteger(1);
+
+    // Идентификатор студента
+    private int id;
+    // Имя студента
+    private String firstName;
+    // Фамилия студента
+    private String lastName;
+    private LocalDateTime birthDay;
+
+    public Student(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = nextId.getAndIncrement();
+    }
+
+    public Student(String firstName, String lastName, LocalDateTime birthDay) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDay = birthDay;
+        this.id = nextId.getAndIncrement();
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public synchronized void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public synchronized void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public LocalDateTime getBirthDay() {
+        return birthDay;
+    }
+
+    public synchronized void setBirthDay(LocalDateTime birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        return Integer.compare(id, o.id);
+    }
 }
